a279682467ab75bd8c98221f5e269c60ad97c9b7
AS <you@example.com>
2019-10-17 16:33:51 +0300

    Initial commit

---
+++ PG/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PG
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            int passwordLength = Convert.ToInt32(passwordLengthText.Text);
+            Random random = new Random();
+            char[] password = new char[passwordLength];
+            string alphabet = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890_";
+            for (int i = 0; i < passwordLength; i++)
+            {
+                password[i] = alphabet[random.Next(0, alphabet.Length - 1)];
+            }
+            resultPassword.Text = resultPassword.Text + "\n" + nameSurname.Text + " - " + new string(password);
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)//запрет на ввод других симоволов
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void ClearField_Click(object sender, RoutedEventArgs e)
+        {
+            resultPassword.Text = "";          
+        }
+        
+        private void SaveToFile_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter saveToFile = new StreamWriter("password.txt")) 
+            {
+                saveToFile.Write(resultPassword.Text);
+            }
+        }
+
+        private void NameSurname_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            nameSurname.Text = "";
+        }
+
+        private void PasswordLengthText_GotFocus(object sender, RoutedEventArgs e)
+        {
+            passwordLengthText.SelectAll();
+        }
+    }
+}
